bd62ec50c0d16a03214fa747586c99c5b9e9de21
Shravani Patil <meetpatilshravani23@gmail.com>
2025-07-10 23:16:00 +0530

    added api key to env

--- src/hooks/useNowPlayingMov.js
+++ src/hooks/useNowPlayingMov.js
@@ -4,13 +4,19 @@ import { useEffect } from "react";
 import { addMovies } from "../utils/movieSlice";
 
 const useNowPlayingMov=()=>{
-    const dispatch=useDispatch();
-const getNowPlayingMov=async()=>{
-    const data= await fetch(url, options);
+     const dispatch =useDispatch();
+   const getNowPlayingMov = async () => {
+   
+  try {
+    const data = await fetch(url, options);
     const json = await data.json();
     console.log(json.results);
     dispatch(addMovies(json.results));
-}
+  } catch (error) {
+    console.error("🔥 Fetch error: ", error);
+  }
+};
+
 useEffect(()=>{
     getNowPlayingMov();
 },[]);
