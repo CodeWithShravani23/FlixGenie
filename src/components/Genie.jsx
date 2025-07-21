25d386860007b16dad990e2e85b2b4cf9279f871
amandollar <toxicempire3@gmail.com>
2025-07-22 00:45:37 +0530

    ehnacment genie

--- src/components/Genie.jsx
+++ src/components/Genie.jsx
@@ -4,18 +4,9 @@ import MoviesbyGenie from './MoviesbyGenie'
 
 const Genie = () => {
   return (
-    <div>
-       <div
-        // className="inset-0 bg-cover bg-center z-30 pointer-events-none"
-        style={{
-          backgroundImage:
-            "url('https://assets.nflxext.com/ffe/siteui/vlv3/05e91faa-6f6d-4325-934e-5418dcc2567b/web/IN-en-20250630-TRIFECTA-perspective_159086b1-425f-435b-bcd5-1ed8039cdef9_large.jpg')",
-        }}
-      />
-      <>
+    <div className=' min-h-screen bg-black'>
        <SearchBar/>
        <MoviesbyGenie/>
-      </>
     </div>
   )
 }
